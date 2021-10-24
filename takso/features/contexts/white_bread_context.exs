ea42183f5fff20475536e5d93901e13afd83d21a
Worraanong Chanchaijak <c.worraanong@gmail.com>
2021-10-24 21:18:52 +0300

    Add customer scenarios

--- takso/features/contexts/white_bread_context.exs
+++ takso/features/contexts/white_bread_context.exs
@@ -19,9 +19,9 @@ defmodule WhiteBreadContext do
     {:ok, state}
   end
 
-  and_ ~r/^I want to go from "(?<argument_one>[^"]+)" to "(?<argument_two>[^"]+)"$/,
-  fn state, %{argument_one: _argument_one,argument_two: _argument_two} ->
-    {:ok, state}
+  and_ ~r/^I want to go from "(?<pickup_address>[^"]+)" to "(?<dropoff_address>[^"]+)"$/,
+  fn state, %{pickup_address: pickup_address, dropoff_address: dropoff_address} ->
+    {:ok, state |> Map.put(:pickup_address, pickup_address) |> Map.put(:dropoff_address, dropoff_address)}
   end
 
   and_ ~r/^I open STRS' web page$/, fn state ->
@@ -30,14 +30,18 @@ defmodule WhiteBreadContext do
   end
 
   and_ ~r/^I enter the booking information$/, fn state ->
+    fill_field({:id, "pickup_address"}, state[:pickup_address])
+    fill_field({:id, "dropoff_address"}, state[:dropoff_address])
     {:ok, state}
   end
 
   when_ ~r/^I summit the booking request$/, fn state ->
+    click({:id, "submit_button"})
     {:ok, state}
   end
 
   then_ ~r/^I should receive a confirmation message$/, fn state ->
+    assert visible_in_page? ~r/Your taxi will arrive in \d+ minutes/
     {:ok, state}
   end
 
