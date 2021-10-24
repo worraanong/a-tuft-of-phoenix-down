a5c823a24d9cb1591d13fdcd527df6a1c99de9f3
Worraanong Chanchaijak <c.worraanong@gmail.com>
2021-10-24 23:06:33 +0300

    BDD - FAIL - Should receive rejection message setup

--- takso/features/contexts/white_bread_context.exs
+++ takso/features/contexts/white_bread_context.exs
@@ -1,6 +1,7 @@
 defmodule WhiteBreadContext do
   use WhiteBread.Context
   use Hound.Helpers
+  alias Takso.{Repo,Sales.Taxi}
 
   feature_starting_state fn  ->
     Application.ensure_all_started(:hound)
@@ -8,14 +9,20 @@ defmodule WhiteBreadContext do
   end
   scenario_starting_state fn _state ->
     Hound.start_session
+    Ecto.Adapters.SQL.Sandbox.checkout(Takso.Repo)
+    Ecto.Adapters.SQL.Sandbox.mode(Takso.Repo, {:shared, self()})
     %{}
   end
   scenario_finalize fn _status, _state ->
+    Ecto.Adapters.SQL.Sandbox.checkin(Takso.Repo)
     # Hound.end_session
     nil
   end
 
-  given_ ~r/^the following taxis are on duty$/, fn state ->
+  given_ ~r/^the following taxis are on duty$/, fn state, %{table_data: table} ->
+    table
+    |> Enum.map(fn taxi -> Taxi.changeset(%Taxi{}, taxi) end)
+    |> Enum.each(fn changeset -> Repo.insert!(changeset) end)
     {:ok, state}
   end
 
@@ -46,6 +53,7 @@ defmodule WhiteBreadContext do
   end
 
   then_ ~r/^I should receive a rejection message$/, fn state ->
+    assert visible_in_page? ~r/At present, there is no taxi available!/
     {:ok, state}
   end
 
