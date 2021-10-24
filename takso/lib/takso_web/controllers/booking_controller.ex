ecfaefe70e6e3ccbab5acf2ba431e7e01703c3f4
Worraanong Chanchaijak <c.worraanong@gmail.com>
2021-10-24 22:51:51 +0300

    BDD - PASS - Taxi will arrive response

--- takso/lib/takso_web/controllers/booking_controller.ex
+++ takso/lib/takso_web/controllers/booking_controller.ex
@@ -1,6 +1,5 @@
 defmodule TaksoWeb.BookingController do
   use TaksoWeb, :controller
-
   def new(conn, _params) do
     render conn, "new.html"
   end
--- takso/lib/takso_web/controllers/user_controller.ex
+++ takso/lib/takso_web/controllers/user_controller.ex
@@ -3,6 +3,7 @@ defmodule TaksoWeb.UserController do
   alias Takso.Repo
   alias Takso.Accounts.User
 
+  @spec index(Plug.Conn.t(), any) :: Plug.Conn.t()
   def index(conn, _params) do
     # render conn, "index.html"
 
