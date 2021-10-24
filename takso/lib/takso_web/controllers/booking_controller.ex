d7db1bb7b95f5822f9326626585827e3eb990d17
Worraanong Chanchaijak <c.worraanong@gmail.com>
2021-10-24 22:36:08 +0300

    TDD - FAIL - Taxi will arrive response

--- takso/lib/takso_web/controllers/booking_controller.ex
+++ takso/lib/takso_web/controllers/booking_controller.ex
@@ -4,4 +4,11 @@ defmodule TaksoWeb.BookingController do
   def new(conn, _params) do
     render conn, "new.html"
   end
+
+  def create(conn, _params) do
+    conn
+    |> put_flash(:info, "Your taxi will arrive in 1 minutes")
+    |> redirect(to: Routes.booking_path(conn, :new))
+  end
+
 end
